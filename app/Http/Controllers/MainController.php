46afe011b2eea4b499444a69476cd75084164965
Валерия-ПК\Валерия <vt@div-art.com>
2018-02-01 11:45:27 +0200

    no message

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,20 +9,8 @@ use App\Product;
 class MainController extends Controller
 {
     public function index(Request $request) {
-        $chunks = [
-            "mebfc" => 3,
-            "mebfc2" => 2,
-            "70g" => 3,
-            "12g" => 3,
-            "200g" => 2,
-            "2000g" => 3
-        ];
 
-        $product = Category::get()->keyBy('code')->each(function($c) use ($chunks) {
-            if ( ! empty($chunks[$c->code])) {
-                $c->product = $c->product->chunk($chunks[$c->code]);
-            }
-        });
+        $product = Category::get()->keyBy('code');
         
         return view('main', [
             'category' => Category::get()->keyBy('code'),
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersSeeder::class);
+        $this->call(UsersSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(ProductSeeder::class);
     }
