b031273efcc46d04b5acec29e460878c54b9a55f
mjs <mjs@div-art.com>
2018-04-11 13:04:54 +0300

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{ env('APP_NAME') }}</title>
         
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
