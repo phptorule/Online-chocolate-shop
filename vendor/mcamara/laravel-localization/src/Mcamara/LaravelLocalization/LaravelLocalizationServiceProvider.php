180105c851a3c2f1a56d69354344207c1aa3ed4f
mjs <mjs@div-art.com>
2018-04-11 12:41:27 +0300

    no message

---
+++ vendor/mcamara/laravel-localization/src/Mcamara/LaravelLocalization/LaravelLocalizationServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Mcamara\LaravelLocalization;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelLocalizationServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../../config/config.php' => config_path('laravellocalization.php'),
+        ], 'config');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['modules.handler', 'modules'];
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $packageConfigFile = __DIR__.'/../../config/config.php';
+
+        $this->mergeConfigFrom(
+            $packageConfigFile, 'laravellocalization'
+        );
+
+        $this->app->singleton(LaravelLocalization::class, function () {
+            return new LaravelLocalization();
+        });
+
+        $this->app->alias(LaravelLocalization::class, 'laravellocalization');
+    }
+}
